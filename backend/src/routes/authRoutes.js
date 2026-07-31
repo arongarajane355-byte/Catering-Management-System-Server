741321befd2559dc43bbcd025e174471a2fbb78b
Ara Jane Arong <arongarajane355@gmail.com>
2026-07-31 17:29:33 +0800

    del
    del

--- backend/src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { login, me } = require("../controllers/authController");
-const authMiddleware = require("../middleware/authMiddleware");
-
-router.post("/login", login);
-router.get("/me", authMiddleware, me);
-
-module.exports = router;
